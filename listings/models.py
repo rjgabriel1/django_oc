64a0f9ebc64dec1698b503831bc27f6c8af2ca20
rjgabriel1 <gabrieljeffersonralph@gmail.com>
2023-08-17 22:24:38 -0400

    initial commit

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Band(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Song(models.Model):
+    title =models.CharField(max_length=150)
--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from listings.models import Band
+from listings.models import Song
 # Create your views here.
+
+
+def hello(request):
+    bands = Band.objects.all()
+    song = Song.objects.all()
+    return render(request,
+        'bands/hello.html',
+        {'bands': bands})
+
+
+def about(request):
+    return HttpResponse("<h1>About Us</h1>")
