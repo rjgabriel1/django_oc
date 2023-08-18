547f3a20bcf67b7dd3a58ecda6f043c0e2f4f75f
rjgabriel1 <gabrieljeffersonralph@gmail.com>
2023-08-18 00:33:46 -0400

    display relevant fields for Band in admin panel

--- listings/admin.py
+++ listings/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from listings.models import Band
 
 # Register your models here.
-
-admin.site.register(Band)
+class BandAndmin(admin.ModelAdmin):
+    # fields to display in admin panel
+    list_display = ('name','year_formed', 'genre')
+    
+    
+admin.site.register(Band,BandAndmin)
